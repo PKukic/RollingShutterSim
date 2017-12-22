09fedce9b38df07ae9a97d404d1da744b6ed17c4
Patrik <patrikkukic@gmail.com>
2017-12-22 22:29:51 +0100

    Added simulation with variable angular velocity

--- Source/PlotData.py
+++ Source/PlotData.py
@@ -4,50 +4,58 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os, os.path
 from mpl_toolkits import mplot3d
 
-# Loading .NPZ file
-data = np.load('../Data/data_phi_ycentr_diff_rolling.npz')
+file_num_range = len([name for name in os.listdir('.') if os.path.isfile(name)])
 
-# Setting array names
-phi_data = data['arr_0']
-ycentr_data = data['arr_1']
-diff_data = data['arr_2']
+print(file_num_range)
+
+for file_n in range(file_num_range-1):
+
+	# Loading .NPZ file
+	data = np.load('../Data/APYD-R/data_apyd_rolling{}.npz'.format(file_n))
+
+	# Setting array names
+	phi_data = data['arr_0']
+	ycentr_data = data['arr_1']
+	diff_data = data['arr_2']
 
 
-### 2D contour graph ###
+	### 2D contour graph ###
 
-# Set plot
-fig, ax = plt.subplots()
-plot = ax.scatter(phi_data, ycentr_data, c=diff_data, cmap = 'inferno')
-cbar = plt.colorbar(plot)
+	# Set plot
+	fig, ax = plt.subplots()
+	plot = ax.scatter(phi_data, ycentr_data, c=diff_data, cmap = 'inferno')
+	cbar = plt.colorbar(plot)
 
-# Legends and labels
-cbar.set_label("Model-centroid point difference")
-ax.set_xlabel("Meteor angle $\phi$ [deg]")
-ax.set_ylabel("Centroid Y coordinate")
+	# Legends and labels
+	cbar.set_label("Model-centroid point difference")
+	ax.set_xlabel("Meteor angle $\phi$ [deg]")
+	ax.set_ylabel("Centroid Y coordinate")
 
-# Configure axis
-plt.axis('tight')
+	# Configure axis
+	plt.axis('tight')
 
-# Saving color plot
-plt.savefig("../Graphs/2DColor_phi_ycentr_diff_rolling.png")
+	# Saving color plot
+	plt.savefig("../Graphs/APYD-R/graph_apyd_rolling{}.png".format(file_n))
 
-plt.show()
+	plt.show()
 
-### 3D plot ### 
+	### 3D plot ### 
+	"""
+	# Set plot
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+	ax.scatter3D(phi_data, ycentr_data, diff_data)
 
-# Set plot
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.scatter3D(phi_data, ycentr_data, diff_data)
+	# Legends and labels
+	ax.set_xlabel("Meteor angle $\phi$")
+	ax.set_ylabel("Centroid Y coordinate")
+	ax.set_zlabel("Model-centroid point difference")
 
-# Legends and labels
-ax.set_xlabel("Meteor angle $\phi$")
-ax.set_ylabel("Centroid Y coordinate")
-ax.set_zlabel("Model-centroid point difference")
+	# Saving 3D plot
+	plt.savefig("../Graphs/3D_phi_ycentr_diff_rolling.png")
 
-# Saving 3D plot
-plt.savefig("../Graphs/3D_phi_ycentr_diff_rolling.png")
-
-plt.show()
+	plt.show()
+	"""
--- Source/RollingShutterSim.py
+++ Source/RollingShutterSim.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
 
     ### Defining function parameters ###
 
+
     # Using rolling shutter
     rolling_shutter = True
 
@@ -25,12 +26,6 @@ if __name__ == "__main__":
     # Meteor angle array
     phi_array = np.arange(0, 361)
 
-    # Meteor's angular velocity (deg/s)
-    omega = 50
-
-    # Angular velocity array
-    omega_arr = np.arange(1, 50.5, 0.5)
-
     # Image size
     img_x = 1280
     img_y = 720
@@ -40,6 +35,12 @@ if __name__ == "__main__":
 
     #  Number of frames per second
     fps = 25
+
+    # Meteor's angular velocity (deg/s)
+    omega = 50
+
+    # Angular velocity array
+    omega_arr = np.arange(1, 50.5, scale)
 
     # Standard deviation along X and Y axis
     sigma_x = 2
@@ -66,50 +67,47 @@ if __name__ == "__main__":
     # pointsCentroidAndModel(*param)
 
 
-    ### Difference as a function of frame number and meteor angle on image ###
+    ### Difference as a function of multiple parameters ###
     
-    # Final data array
-    phi_num_diff_array = []
+    num = 0
+
+    for omega_iter in omega_arr:
+
+        # Final data array
+        phi_ycentr_diff_array = []
+
+        for phi_iter in phi_array:
+
+            # LIST of centroid and model coordinates
+            centroid_coordinates, model_coordinates = pointsCentroidAndModel(rolling_shutter, t_meteor, phi_iter, omega_iter, img_x, img_y, scale, fps, sigma_x, sigma_y, noise_scale, offset, show_plots)
+            
+            # Size of frame number array
+            frame_num_range = len(centroid_coordinates)
+
+            print("Number of frames: {};".format(frame_num_range))
+
+            # Generating and appending model-centroid points difference
+            for frame_num in range(frame_num_range):
+
+                # Parameters
+                diff = centroidDifference(centroid_coordinates[frame_num], model_coordinates[frame_num])
+                y_centr = centroid_coordinates[frame_num][1]
+                
+                # Checking parameters
+                print("Velocity: {:.2f} Angle: {:.2f}; Y coordinate: {:.2f}; Difference: {:.2f};".format(omega_iter, phi_iter, y_centr, diff))
+
+                phi_ycentr_diff_array.append((phi_iter, y_centr, diff))
 
 
-    for phi_iter in phi_array:
+        # Variable arrays
+        phi_data = [point[0] for point in phi_ycentr_diff_array]
+        ycentr_data = [point[1] for point in phi_ycentr_diff_array]
+        diff_data = [point[2] for point in phi_ycentr_diff_array]
 
-        # LIST of centroid and model coordinates
-        centroid_coordinates, model_coordinates = pointsCentroidAndModel(rolling_shutter, t_meteor, phi_iter, omega, img_x, img_y, scale, fps, sigma_x, sigma_y, noise_scale, offset, show_plots)
-        
-        # Size of frame number array
-        frame_num_range = len(centroid_coordinates)
+        # Checking
+        print(len(phi_data), len(ycentr_data), len(diff_data))
 
-        # Generating and appending model-centroid points difference
-        for frame_num in range(frame_num_range):
-            diff = centroidDifference(centroid_coordinates[frame_num], model_coordinates[frame_num])
-            phi_num_diff_array.append((phi_iter, frame_num, diff))
+        # Saving data
+        np.savez('../Data/APYD-R/data_apyd_rolling{}.npz'.format(num), *[phi_data, ycentr_data, diff_data])
 
-            # Checking parameters
-            print("Meteor angle: {} frame number: {} difference: {:.2f}".format(phi_iter, frame_num, diff))
-
-
-    # Variable arrays
-    phi_data = [point[0] for point in phi_num_diff_array]
-    frame_num_data = [point[1] for point in phi_num_diff_array]
-    diff_data = [point[2] for point in phi_num_diff_array]
-
-    # Generate frame number array
-    frame_num_size = len(frame_num_data)/phi_array.size
-    frame_array = np.arange(frame_num_size)
-    
-    # Generate frame number/angle meshgrid
-    pp, ff = np.meshgrid(phi_array, frame_array)
-
-    # Reshape the difference array so that it is compatible with the meshgrid
-    diff_data = np.reshape(diff_data, (phi_array.size, frame_num_size))
-
-    
-    # Checking sizes
-    print("Size of frame num array: {}".format(frame_array.size))
-    print("Size of phi array: {}".format(phi_array.size))
-    print("Shape of difference data: {}".format(diff_data.shape))
-
-
-    # Saving data
-    np.savez('data_phi_frame_diff_rolling.npz', *[pp, ff, diff_data])
+        num += 1
