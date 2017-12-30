a19cbd6a9ee8a44c0c24aaaa09e46cf48cfeeef4
Patrik <patrikkukic@gmail.com>
2017-12-30 09:43:14 +0100

    fixed bugs

--- Source/RollingShutterSim.py
+++ Source/RollingShutterSim.py
@@ -75,56 +75,55 @@ param = [rolling_shutter, t_meteor, phi, omega, img_x, img_y, scale, fps, sigma_
 num_omega = 0
 for omega_iter in omega_arr:
 
-    if omega_iter == omega_arr[len(omega_arr) - 1]:
+    
+    # Final data array
+    phi_ycentr_diff_array = []
+    # amplitude = 255/img_y*(2*omega_iter/scale)
+    # print("Amplitude: {}".format(amplitude))
 
-        # Final data array
-        phi_ycentr_diff_array = []
-        # amplitude = 255/img_y*(2*omega_iter/scale)
-        # print("Amplitude: {}".format(amplitude))
+    for phi_iter in phi_array:
 
-        for phi_iter in phi_array:
+        # LIST of centroid and model coordinates
+        centroid_coordinates, model_coordinates = pointsCentroidAndModel(rolling_shutter, t_meteor, phi_iter, omega_iter, img_x, img_y, scale, fps, sigma_x, sigma_y, noise_scale, offset, show_plots)
+    
 
-            # LIST of centroid and model coordinates
-            centroid_coordinates, model_coordinates = pointsCentroidAndModel(rolling_shutter, t_meteor, phi_iter, omega_iter, img_x, img_y, scale, fps, sigma_x, sigma_y, noise_scale, offset, show_plots)
-        
+        # Size of frame number array
+        frame_num_range = len(centroid_coordinates)
 
-            # Size of frame number array
-            frame_num_range = len(centroid_coordinates)
+        print("Number of frames: {};".format(frame_num_range))
 
-            print("Number of frames: {};".format(frame_num_range))
+        # Generating and appending model-centroid points difference
+        for frame_num in range(frame_num_range):
 
-            # Generating and appending model-centroid points difference
-            for frame_num in range(frame_num_range):
+            # Parameters
+            diff = centroidDifference(centroid_coordinates[frame_num], model_coordinates[frame_num])
+            if(diff > 250):
+                print("DIFFERENCE GREATER THAN 250 PX")
 
-                # Parameters
-                diff = centroidDifference(centroid_coordinates[frame_num], model_coordinates[frame_num])
-                if(diff > 250):
-                    print("DIFFERENCE GREATER THAN 250 PX")
+            y_centr = centroid_coordinates[frame_num][1]
+            
+            # Checking parameters
+            print("Velocity: {:.2f} Angle: {:.2f}; Y coordinate: {:.2f}; Difference: {:.2f};".format(omega_iter, phi_iter, y_centr, diff))
+            
+            # Checking coordinates
+            print("\tCentroid coordinates: ({:.2f}, {:.2f})".format(centroid_coordinates[frame_num][0], \
+                centroid_coordinates[frame_num][1]))
 
-                y_centr = centroid_coordinates[frame_num][1]
-                
-                # Checking parameters
-                print("Velocity: {:.2f} Angle: {:.2f}; Y coordinate: {:.2f}; Difference: {:.2f};".format(omega_iter, phi_iter, y_centr, diff))
-                
-                # Checking coordinates
-                print("\tCentroid coordinates: ({:.2f}, {:.2f})".format(centroid_coordinates[frame_num][0], \
-                    centroid_coordinates[frame_num][1]))
+            print("\tModel coordinates: ({:.2f}, {:.2f})".format(model_coordinates[frame_num][0], \
+                model_coordinates[frame_num][1]))
 
-                print("\tModel coordinates: ({:.2f}, {:.2f})".format(model_coordinates[frame_num][0], \
-                    model_coordinates[frame_num][1]))
-
-                phi_ycentr_diff_array.append((phi_iter, y_centr, diff))
+            phi_ycentr_diff_array.append((phi_iter, y_centr, diff))
 
 
-        # Variable arrays
-        phi_data = [point[0] for point in phi_ycentr_diff_array]
-        ycentr_data = [point[1] for point in phi_ycentr_diff_array]
-        diff_data = [point[2] for point in phi_ycentr_diff_array]
+    # Variable arrays
+    phi_data = [point[0] for point in phi_ycentr_diff_array]
+    ycentr_data = [point[1] for point in phi_ycentr_diff_array]
+    diff_data = [point[2] for point in phi_ycentr_diff_array]
 
-        # Checking
-        print(len(phi_data), len(ycentr_data), len(diff_data))
+    # Checking
+    print(len(phi_data), len(ycentr_data), len(diff_data))
 
-        # Saving data
-        np.savez('../Data/OPYD-R/data_opyd_rolling{}.npz'.format(num_omega), *[omega_pxs, phi_data, ycentr_data, diff_data])
+    # Saving data
+    np.savez('../Data/OPYD-R/data_opyd_rolling{}.npz'.format(num_omega), *[omega_pxs, phi_data, ycentr_data, diff_data])
 
     num_omega += 1
