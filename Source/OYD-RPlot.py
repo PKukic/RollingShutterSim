d2154f4dcaebfaa0cb0e05e0ec7c452bd58f79e2
Patrik <patrikkukic@gmail.com>
2018-01-03 15:53:20 +0100

    3d plot + plot code

--- Source/OYD-RPlot.py
+++ Source/OYD-RPlot.py
@@ -2,12 +2,13 @@
 """
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import Parameters as par
 
 
 # Loading .NPZ file
-data = np.load('../Data/OYD-R/data_oyd_rolling_90.npz')
+data = np.load('../Data/OYD-R/data_oyd_rolling_45.npz')
 
 # Setting array names
 omega_arr = data['arr_0']
@@ -15,12 +16,13 @@ ycentr_data = data['arr_1']
 diff_data = data['arr_2']
 
 # Set plot
-fig, ax = plt.subplots()
-plot = ax.scatter(omega_arr, ycentr_data, c = diff_data, cmap = 'inferno', lw = 0)
-cbar = plt.colorbar(plot)
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(omega_arr, ycentr_data, diff_data, c = diff_data, cmap = 'inferno', lw = 0)
+
 
 # Legends and labels
-cbar.set_label("Model-centroid  point difference [px]")
+ax.set_zlabel("Model-centroid  point difference [px]")
 ax.set_xlabel("Angular velocity $\omega$ [px/s]")
 ax.set_ylabel("Centroid Y coordinate [px]")
 plt.title("Meteor angle fixed to: {} [deg]".format(par.phi))
@@ -28,6 +30,6 @@ plt.title("Meteor angle fixed to: {} [deg]".format(par.phi))
 # Configure axis
 plt.axis('tight')
 
-plt.savefig("../Graphs/OYD-R/graph_oyd_rolling_90.png")
+plt.savefig("../Graphs/OYD-R/graph_oyd_rolling_45(2).png")
 
 plt.show()
