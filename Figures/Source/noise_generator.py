3b9f8711eb9ea63069ccb65b5e606105e8b62679
Patrik <patrikkukic@gmail.com>
2018-06-27 18:03:56 +0200

    improved plotting

--- Figures/Source/noise_generator.py
+++ Figures/Source/noise_generator.py
@@ -1,12 +1,21 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-img_y = img_x = 1000
+img_y = img_x = 200
 noise_scale = [0.01, 5, 10, 20]
+offset = 50
 
-for i in range(len(noise_scale)):
+fig, axes = plt.subplots(ncols=len(noise_scale))
 
-	plt.imshow(np.random.normal(loc=0, scale=noise_scale[i], size=(img_y, img_x)), cmap='gray', vmin=0, vmax=255)
-	plt.title(r"$\sigma$ = {}".format(noise_scale[i] if noise_scale[i] != 0.01 else 0))
-	plt.axis("off")
-	plt.show()
+for i, ax in zip(range(len(noise_scale)), axes):
+
+	ax.imshow(offset + np.abs(np.random.normal(loc=0, scale=noise_scale[i], size=(img_y, img_x))), cmap='gray', vmin=0, vmax=255)
+	
+	ax.set_title(r"$\sigma$ = {}".format(noise_scale[i] if noise_scale[i] != 0.01 else 0))
+
+	ax.axis("off")
+
+
+plt.tight_layout()
+plt.subplots_adjust(wspace=0.1)
+plt.show()
