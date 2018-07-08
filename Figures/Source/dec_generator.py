1b25f23c36d4eec62e2be49cfd8031bd30d5608c
Patrik <patrikkukic@gmail.com>
2018-07-08 12:06:20 +0200

    changed first deceleration parameter to ~1e-3

--- Figures/Source/dec_generator.py
+++ Figures/Source/dec_generator.py
@@ -14,8 +14,9 @@ fin = omega_start*0.21
 step = omega_start*0.02
 delta_omega = np.arange(start, fin, step)
 
+print(delta_omega)
 
-a = 1.1
+a = 1.1/1000
 b_arr = []
 
 fin_t = 0.5
@@ -23,6 +24,7 @@ dt = 0.005
 time_arr = np.arange(0, fin_t, dt)
 
 for i in range(len(delta_omega)):
+    print(omega_start - delta_omega[i])
     b_arr.append(st.getparam(a, omega_start, omega_start - delta_omega[i], fin_t))
 
 print(b_arr)
@@ -38,6 +40,9 @@ for i in range(len(delta_omega)):
     for t in time_arr:
 
         omega_arr_iter.append((omega_start - a*b_iter*np.exp(b_iter*t))*par.scale)
+
+    if i == 0:
+        print(omega_arr_iter)
 
     omega_arr.append(omega_arr_iter)
 
