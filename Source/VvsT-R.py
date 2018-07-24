5ffedea4f4645a04493d3378077fcc08fb9b218f
Patrik Kukic <patrikkukic@gmail.com>
2018-07-25 00:14:07 +0200

    modified VvsT

--- Source/VvsT-R.py
+++ Source/VvsT-R.py
@@ -2,6 +2,7 @@ import SimulationTools as st
 import Parameters as par
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 rolling_shutter = True
 show_plots = False
@@ -20,20 +21,29 @@ print(time_rolling)
 print(centroids_rolling)
 
 dist_rolling, velocity_rolling = st.getVelocity(time_rolling, centroids_rolling)
+dist_model, velocity_model = st.getVelocity(time_rolling, model_rolling)
 
-plt.plot(time_rolling, velocity_rolling)
-plt.ylim((1300-650, 1300+650))
+plt.plot(time_rolling, velocity_rolling, label = 'rolling')
+plt.plot(time_rolling, velocity_model, label = 'model')
+
+avg_avg = np.average([np.average(velocity_rolling), np.average(velocity_model)])
+plt.ylim((avg_avg-100, avg_avg+100))
 
 plt.xlabel('Time [s]')
 plt.ylabel('Velocity [px/s]')
 
+plt.legend(loc = 'best')
+
 plt.savefig('/home/patrik/Desktop/t_vel.png')
 plt.show()
 
-plt.plot(time_rolling, dist_rolling)
+plt.plot(time_rolling, dist_rolling, label = 'rolling')
+plt.plot(time_rolling, dist_model, label = 'model')
 
 plt.xlabel('Time [s]')
 plt.ylabel('Distance from the starting centroid [px]')
+
+plt.legend(loc = 'best')
 
 plt.savefig('/home/patrik/Desktop/t_dist.png')
 plt.show()
